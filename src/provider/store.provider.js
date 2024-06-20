5a5ab66a002dcceaedf24a799fb901c52ec81111
OSSU <o.ss.u2056@gmail.com>
2024-06-21 03:17:12 +0900

    feat(#14): 가게 미션 목록 조회 provider

--- src/provider/store.provider.js
+++ src/provider/store.provider.js
@@ -1,7 +1,12 @@
-import { getStoreReviewResponseDTO } from "../dtos/store.dto.js";
-import { getStoreReviews, getStoreReviewsCount } from "../models/store.dao.js";
+import { getStoreMissionResponseDTO, getStoreReviewResponseDTO } from "../dtos/store.dto.js";
+import { getStoreMissions, getStoreMissionsCount, getStoreReviews, getStoreReviewsCount } from "../models/store.dao.js";
 
 export const getStoreReview = async (storeId, query) => {
     const {reviewId, size = 3} = query;
     return getStoreReviewResponseDTO(await getStoreReviews(storeId, reviewId, size), await getStoreReviewsCount(storeId));
+}
+
+export const getStoreMission = async (storeId, query) => {
+    const {page, size = 3} = query;
+    return getStoreMissionResponseDTO(await getStoreMissions(storeId, page, size ), await getStoreMissionsCount(storeId));
 }
